deceb30a283b76b88e92a5994010cda0d77aa770
Siddharth <sid.8960266720@gmail.com>
2026-02-07 17:56:27 +0530

    Project structure ready and virtual environment set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sid",
+    author_email="sid.8960266720@gmail.com",
+    packages=find_packages()
+)
